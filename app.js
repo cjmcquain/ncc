421380d42cfa4a87a74a25fc88c3889813868cab
Charles McQuain <charlesmcquain@gmail.com>
2020-09-03 20:28:31 -0700

    Added partial views, included partials in all view pages

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require('express');
 
+// express app
 const app = express();
 
+// listen for requests
+app.listen(3000);
+
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
 app.get('/', (req, res) => {
-    //res.send("<h1>Welcome</h1><p>This is my website</p>");
-    res.sendfile('./views/index.html', { root: __dirname });
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    //res.send("<h1>About page</h1><p>This is my website</p>");
-    res.sendfile('./views/about.html', { root: __dirname });
+  res.render('about', { title: 'About' });
 });
 
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
+// 404 page
 app.use((req, res) => {
-    res.sendFile('./views/404.html', { root: __dirname });
-});
-
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+  res.status(404).render('404', { title: '404' });
 });
