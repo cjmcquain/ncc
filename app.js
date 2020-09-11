39aafc3e8d4973f80714b58961ff4ed6c3282837
Charles McQuain <charlesmcquain@gmail.com>
2020-09-10 21:56:51 -0700

    Added mongoose

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
-
-app.use(morgan('dev'));
+const dbURI = 'mongodb+srv://netninja:Test1234@cluster0.cqpkb.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err) );
 
 // register view engine
 app.set('view engine', 'ejs');
 // app.set('views', 'myviews');
 
-app.use((req, res, next) => {
-    console.log('request made');
-    console.log('host: ', req.hostname);
-    console.log('path: ', req.path);
-    console.log('method: ', req.method);
-    next();
-});
+// middleware and static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   const blogs = [
