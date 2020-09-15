5e86b588a7451bb40aca431cad8051192d860d7f
Charles McQuain <charlesmcquain@gmail.com>
2020-09-14 20:41:49 -0700

    Added ability to create blogs and save to db

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set('view engine', 'ejs');
 
 // middleware and static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
 
 
@@ -34,6 +35,18 @@ app.get('/blogs', (req, res) => {
   Blog.find()
     .then((result) => {
       res.render('index', { title: 'All Blogs', blogs: result})
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs');
     })
     .catch((err) => {
       console.log(err);
